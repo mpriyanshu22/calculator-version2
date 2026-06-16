5bc91773f599b530115477f18600775926ded33a
Priyanshu MIshra <iitianki22@gmail.com>
2026-06-17 01:31:29 +0530

    Update ButtonsContainer.jsx

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,7 +1,7 @@
 import styles from './ButtonsContainer.module.css'
 const ButtonsContainer=({onButtonClick})=>{
   const buttonNames=[
-  'c','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.','!'
+  'C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.','!'
   ];
 return <div className={styles.buttonContainer}>
   {buttonNames.map(buttonName=>  <button className={styles.button} key={buttonName}
